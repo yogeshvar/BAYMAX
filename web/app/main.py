5a3498969b9c76ccf497535ea09bc04b06bff4b9
Maggie Yogeshvar <yogeshvar@icloud.com>
2022-12-12 20:31:26 +1030

    ⚡️ improve code standard

--- web/app/main.py
+++ web/app/main.py
@@ -1,19 +1,34 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from transformers import AutoTokenizer, AutoModelWithLMHead
-
-
+from dotenv import dotenv_values
 
 app = Flask(__name__)
-CORS(app, resources={r"/runModel": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+
+access_token = dotenv_values("../.env")['access_token']
 
 tokenizer = AutoTokenizer.from_pretrained('gpt2-medium')
 model = AutoModelWithLMHead.from_pretrained(
-    'model/model/')
+    dotenv_values('../.env')['model_name'], use_auth_token=access_token)
+
 
 @app.route("/")
 def hello_world():
     return "Hello, World! From Flask"
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin',
+                         '*')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    return response
+
 
 @app.route("/runModel", methods=['POST'])
 @cross_origin()
@@ -26,7 +41,7 @@ def runModel():
     response_post = req['response']
     return_original = req['return_original']
     input_ids = tokenizer.encode(seeker_post, return_tensors='pt')
-    output = model.generate(input_ids, max_length=100, 
+    output = model.generate(input_ids, max_length=100,
                             repetition_penalty=1.2,
                             pad_token_id=tokenizer.eos_token_id,
                             no_repeat_ngram_size=3,
